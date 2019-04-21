c08011a791583556a3c517a423948e8a39f51181
Administrador <administrador@MacBook-Pro-de-Administrador.local>
2019-04-21 17:07:26 -0400

    residual commit

--- src/components/PlayersList.js
+++ src/components/PlayersList.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
 const PlayersList = props => {
 	if (!props.players){
 		return (
@@ -14,14 +21,38 @@ const PlayersList = props => {
 	}
 
 	return (
+		<>
+		<Paper >
+		  <Table >
+		    <TableHead>
+		      <TableRow>
+		        <TableCell>Name</TableCell>
+		        <TableCell >Country</TableCell>
+		        <TableCell >Jersey Number</TableCell>
+		        <TableCell >Date of birth</TableCell>
+		        <TableCell >Position</TableCell>
+		        <TableCell >Contract until</TableCell>
+		      </TableRow>
+		    </TableHead>
+		    <TableBody>
+		      {Array.prototype.map.call(props.players, (player, i)=>(
+		      	<TableRow >
+		      	  <TableCell component="th" scope="row">
+		      	    {player.name}
+		      	  </TableCell>
+		      	  <TableCell align="right">{player.nationality}</TableCell>
+		      	  <TableCell align="right">{player.jerseyNumber}</TableCell>
+		      	  <TableCell align="right">{player.dateOfBirth}</TableCell>
+		      	  <TableCell align="right">{player.position}</TableCell>
+		      	  <TableCell align="right">{player.contractUntil}</TableCell>
+		      	</TableRow>
+		      )
 
-		<div>
-			{
-				Array.prototype.map.call(props.players, (player, i)=>{
-				  return <p key={i}>Nombre:{player.name}, Pais: {player.nationality}, Numero de camiseta: {player.jerseyNumber}</p>
-				})
-			}
-		</div>
+		      )}
+		    </TableBody>
+		  </Table>
+		</Paper>
+		</>
 	)
 }
 
