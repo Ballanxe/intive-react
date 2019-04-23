d0fad4f18681fc5242f31ba061ff9ccc71700fa1
Administrador <administrador@MacBook-Pro-de-Administrador.local>
2019-04-22 23:56:15 -0400

    giving some style to the search form

--- src/components/PlayersSearch.js
+++ src/components/PlayersSearch.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import TextField from '@material-ui/core/TextField';
-import MenuItem from '@material-ui/core/MenuItem';
+import { TextField, MenuItem, Grid, Button } from '@material-ui/core';
+// import MenuItem from '@material-ui/core/MenuItem';
 
 import * as playersAction  from '../actions/players';
 
@@ -12,6 +12,7 @@ class PlayersSearch extends React.Component{
 
 	state = {
 		positions: [
+			"",
 			"Keeper",
 			"Attacking Midfield",
 			"Left-Back",
@@ -26,7 +27,6 @@ class PlayersSearch extends React.Component{
 		]
 	}
 
-
 	updateFieldEvent = key => ev => {
 
 		console.log({
@@ -37,83 +37,80 @@ class PlayersSearch extends React.Component{
 			[key]:ev.target.value
 		});
 	} 
-
-
 	changeName = this.updateFieldEvent('player_name')
 	changePosition = this.updateFieldEvent('position')
 	changeAge = this.updateFieldEvent('number')
-
 	onSubmitForm = (e) => {
 		e.preventDefault()
 		this.props.onSubmitForm({
-			players_name:this.props.player_name,
+			players_name:this.props.player_name == "" ? null : this.props.player_name,
 			position: this.props.position,
-			age: this.props.number
+			age: this.props.number == "" ? null : this.props.number
 		})
 	}
-
-
 	render(){
 
 		return(
-			<div className="container">
-				<div className="row">
-					<form noValidate autoComplete="off">
-						<TextField
-						  id="outlined-full-width"
-						  label="Player name"
-						  style={{ margin: 8 }}
-						  placeholder="Insert player name"
-						  fullWidth
-						  margin="normal"
-						  variant="outlined"
-						  onChange={this.changeName}
-						  InputLabelProps={{
-						    shrink: true,
-						  }}
-						/>
-						<TextField
-						  id="outlined-select-position"
-						  select
-						  label="PPosition"
-						  style={{
-						  	width:100,
-						  	color:"#ffffff",
-						  }}
-						  fullWidth
-						  value={this.props.position}
-						  onChange={this.changePosition}
-						  variant="outlined"
-						>
-					  {this.state.positions.map(position => (
-						    <MenuItem key={position} value={position}>
-						      {position}
-						    </MenuItem>
-						  ))}
-						</TextField>
-						<TextField
-						  id="outlined-full-width"
-						  label="Age"
-						  style={{ margin: 8 }}
-						  placeholder="Insert player's age"
-						  fullWidth
-						  margin="normal"
-						  variant="outlined"
-						  onChange={this.changeAge}
-						  InputLabelProps={{
-						    shrink: true,
-						  }}
-						/>
-						<button
-						  className="btn btn-primary"
-						  type="submit"
-						  disabled=""
-						  onClick={this.onSubmitForm}>
-						  Search
-						</button>
-					</form>
-				</div>
-			</div>
+			<Grid container style={{ marginTop:20, marginBottom:20 }}>
+				<form noValidate autoComplete="off" style={{ width:100+'%' }}>
+					<Grid item style={{ display:'flex', flexWrap:'nowrap', alignItems:"center"}}>
+					<TextField
+					  id="outlined-full-width"
+					  label="Player name"
+					  style={{ margin: 8 }}
+					  placeholder="Insert player name"
+					  fullWidth
+					  margin="normal"
+					  variant="outlined"
+					  onChange={this.changeName}
+					  InputLabelProps={{
+					    shrink: true,
+					  }}
+					/>
+					<TextField
+					  id="outlined-select-position"
+					  select
+					  label="Position"
+					  style={{
+					  	color:"#ffffff",
+					  	margin: 8
+					  }}
+					  fullWidth
+					  value={this.props.position}
+					  onChange={this.changePosition}
+					  variant="outlined"
+					>
+				  {this.state.positions.map(position => (
+					    <MenuItem key={position} value={position}>
+					      {position}
+					    </MenuItem>
+					  ))}
+					</TextField>
+					<TextField
+					  id="outlined-full-width"
+					  label="Age"
+					  style={{ margin: 8 }}
+					  placeholder="Insert player's age"
+					  fullWidth
+					  margin="normal"
+					  variant="outlined"
+					  onChange={this.changeAge}
+					  InputLabelProps={{
+					    shrink: true,
+					  }}
+					/>
+					<Button 
+						variant="contained"
+						color="primary"
+						type="submit"
+						onClick={this.onSubmitForm}
+						style={{ width:300 }}>
+					  Search
+					</Button>
+					</Grid>
+				</form>
+			</Grid>
+			
 		)
 	}
 
